dfdbbbb1136b2f6817bbb392e66b591c53b86f5f
Daniel Schmidt <dschmidt@mesosphere.com>
2019-06-19 23:42:54 +0200

    work in progress implementation of ast based commenting approach

--- src/fixTypescriptErrors.ts
+++ src/fixTypescriptErrors.ts
@@ -1,22 +1,91 @@
-import { Logger } from "./logger";
-import * as ts from "typescript";
 import fs from "fs";
+import * as ts from "typescript";
+import * as parser from "@babel/parser";
+import traverse, { NodePath } from "@babel/traverse";
+import generate from "@babel/generator";
+import * as t from "@babel/types";
+import { Logger } from "./logger";
 
-const disableLine = "// ts-ignore typescript-all-in";
-export function fixError(diagnostic: ts.Diagnostic, logger: Logger) {
-  const file = diagnostic.file as ts.SourceFile;
+function fixErrorsForFile(diagnosticList: ts.Diagnostic[], logger: Logger) {
+  if (!diagnosticList.length) {
+    return;
+  }
+
+  const file = diagnosticList[0].file as ts.SourceFile;
   const fileName = file.fileName;
-  const fileContent = fs.readFileSync(fileName, "utf-8");
+  // get positions
+  const errorPositions = diagnosticList
+    .filter(diagnostic => typeof diagnostic.start === "number")
+    .map(diagnostic => diagnostic.start as number);
 
-  const lines = fileContent.split("\n");
-  const { line } = file.getLineAndCharacterOfPosition(diagnostic.start || 0);
+  // parse the file
+  const ast = parser.parse(fs.readFileSync(fileName, "utf-8"), {
+    sourceType: "unambiguous",
+    plugins: [
+      "typescript",
+      "jsx",
+      "dynamicImport",
+      "decorators-legacy",
+      "classProperties"
+    ]
+  });
 
-  // TODO: this is too naive, we need to find the right place in the code to add it
-  // For this we need to parse the TS file and insert the comment before
-  lines.splice(line, 0, disableLine);
-  const newFileContent = lines.join("\n");
+  logger.log(`Parsed ${fileName}`);
 
-  fs.writeFileSync(fileName, newFileContent);
+  // find all ast nodes
+  const errorAstPaths = errorPositions
+    .map(position => {
+      let result: NodePath<t.Expression>;
+
+      traverse(ast, {
+        Expression: function(path) {
+          const node = path.node;
+
+          if (!result) {
+            result = path;
+          }
+          // We are searching for the last expression that ends before the error
+
+          /**
+           * 1
+           * 2
+           * 3 position
+           * 4 node
+           */
+          if ((node.start || Infinity) > position) {
+            return;
+          }
+
+          /**
+           * 1 node
+           * 2 result
+           * 3 position
+           * 4
+           */
+          if ((node.end || Infinity) < (result.node.end || Infinity)) {
+            return;
+          }
+
+          result = path;
+        }
+      });
+
+      // @ts-ignore
+      return result;
+    })
+    .filter(result => result !== null);
+
+  logger.log(`Adding ${errorAstPaths.length} comments to ${fileName}`);
+
+  // update ast nodes
+  errorAstPaths.forEach(path => {
+    path.addComment("CommentLine", "// @ts-ignore typescript-all-in", true);
+  });
+
+  // save the file
+  const code = generate(ast).code;
+  logger.log(`Writing ${fileName}`);
+  fs.writeFileSync(fileName, code, "utf-8");
 }
 
 export default function detectAndFixTypescriptErrors(
@@ -56,16 +125,21 @@ export default function detectAndFixTypescriptErrors(
 
   const { diagnostics } = program.emit();
 
-  let allDiagnostics = ts.getPreEmitDiagnostics(program).concat(diagnostics);
+  const allDiagnostics = ts.getPreEmitDiagnostics(program).concat(diagnostics);
 
+  const fileNameMap: Record<string, ts.Diagnostic[]> = {};
   allDiagnostics.forEach(diagnostic => {
     if (diagnostic.file) {
-      // TODO: multithread this part
-      // TODO: do other errors disappear if one error is ignored?
-      fixError(diagnostic, logger);
+      const fileName = diagnostic.file.fileName;
+      fileNameMap[fileName] = fileNameMap[fileName] || [];
+      fileNameMap[fileName].push(diagnostic);
     } else {
       const msg = ts.flattenDiagnosticMessageText(diagnostic.messageText, "\n");
       logger.log(`Ignoring error without file: "${msg}"`);
     }
+  });
+
+  Object.values(fileNameMap).forEach(diagnosticsList => {
+    fixErrorsForFile(diagnosticsList, logger);
   });
 }
