84cc887b587ff4ad687f7516b663cf8b5da03dfd
Daniel Schmidt <dschmidt@mesosphere.com>
2019-07-11 10:27:04 +0200

    test: add helper for typescript fixing

--- src/__tests__/fixTypescriptErrors-test.ts
+++ src/__tests__/fixTypescriptErrors-test.ts
@@ -1,6 +1,7 @@
 import path from "path";
 import shell from "shelljs";
 import fs from "fs";
+import tmp from "tmp"; // @ts-ignore typescript-all-in
 import detectAndFixTypescriptErrors from "../fixTypescriptErrors";
 
 const nullLogger = {
@@ -8,26 +9,52 @@ const nullLogger = {
   info: () => {}
 };
 
+const defaultTsConfig = {
+  compilerOptions: {
+    target: "es5",
+    module: "commonjs",
+    sourceMap: true,
+    strict: true,
+    esModuleInterop: true
+  }
+};
+
+function runFixer(input: string, tsConfig?: Object): string {
+  const tmpFolder = tmp.dirSync();
+  const tmpFolderPath = tmpFolder.name;
+  const filePath = path.resolve(tmpFolderPath, "index.ts");
+  const tsConfigPath = path.resolve(tmpFolderPath, "tsconfig.json");
+
+  fs.writeFileSync(filePath, input, "utf-8");
+  fs.writeFileSync(
+    tsConfigPath,
+    JSON.stringify(tsConfig || defaultTsConfig),
+    "utf-8"
+  );
+
+  detectAndFixTypescriptErrors(tmpFolderPath, [], nullLogger);
+  const content = fs.readFileSync(filePath, "utf-8");
+  tmpFolder.removeCallback();
+  return content;
+}
+
 describe("fixTypescriptErrors", () => {
   it("supresses typescript error in simple project", () => {
-    const source = path.resolve(__dirname, "../simpleFixture/index");
-    const target = path.resolve(__dirname, "../simpleFixture/index.ts");
-
-    shell.cp(source, target);
-
-    detectAndFixTypescriptErrors(target, [], nullLogger);
-
-    const fileContent = fs.readFileSync(target, "utf-8");
-    expect(fileContent).toMatchInlineSnapshot(`
+    expect(
+      runFixer(`
+    function foo(str: string): number {
+      return (
+        3 * str
+      );
+    }
+    `)
+    ).toMatchInlineSnapshot(`
       "function foo(str: string): number {
         return (
-          // @ts-ignore typescript-all-in
-          3 * str
+          3 * str // @ts-ignore typescript-all-in
         );
       }
       "
     `);
-
-    shell.rm(target);
   });
 });
